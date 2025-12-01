5b3e4d0408b382d829ea1e05d24101a3f7ee4d8d
Valerie Kristine Taberdo <taberdovk@gmail.com>
2025-12-01 19:16:00 +0800

    feat/create-table-animals

--- veterinary.sql
+++ veterinary.sql
@@ -9,3 +9,14 @@ CREATE TABLE owners(
     email VARCHAR(100),
 );
 
+CREATE TABLE animals(
+    animalid INT PRIMARY KEY,
+    name VARCHAR(50),
+    species VARCHAR(50),
+    breed VARCHAR(50),
+    dateofbirth DATE,
+    gender VARCHAR(10),
+    color VARCHAR(50),
+    ownerid INT,
+    FOREIGN KEY (ownerid) REFERENCES owners(ownerid)
+);
