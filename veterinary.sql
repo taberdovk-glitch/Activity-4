d4e5f39aa7bf82f2b1d5c769375c81dbe1cce441
Valerie Kristine Taberdo <taberdovk@gmail.com>
2025-12-01 22:14:10 +0800

    feat/insert-10-rows-doctors

--- veterinary.sql
+++ veterinary.sql
@@ -97,4 +97,12 @@ VALUES
 (9, 9, '2025-12-05', 'Allergy'),
 (10, 10, '2025-12-10', 'Conjunctivitis');
 
+INSERT INTO doctors(doctorid, dfirstname, dlastname, speciality,phone, email) 
+VALUES
+(1, 'Dr.Maria', 'Santos', 'General Veterinarian', '987-654-3210', 'maria@example.com'),
+(2, 'Dr.Antonio', 'Gonzales', 'Feline Specialist', '555-123-4567', 'antonio@example.com'),
+(3, 'Dr.Felipe', 'Luna', 'Orthopedic Specialist', '111-222-3333', 'felipe@example.com'),
+(4, 'Dr.Sofia', 'Reyes', 'Dermatology Specialist', '999-888-7777', 'sofia@xample.com'),
+(5, 'Dr.Luis', 'Torres', 'Surgery Specialist', '123-555-7777', 'luis@example.com'),
+(6, 'Dr.Carmen', 'Fernandez', 'Opthalmology Specialist', '333-222-1111', 'carmen@example.com');
 
