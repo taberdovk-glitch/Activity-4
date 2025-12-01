1f6c84fb433c726bf07bfb4589d7a1c76062c235
Valerie Kristine Taberdo <taberdovk@gmail.com>
2025-12-01 23:20:26 +0800

    feat/add-column-registereddate

--- veterinary.sql
+++ veterinary.sql
@@ -132,3 +132,5 @@ VALUES
 (9, 9, '2023-05-02 00:00:00', 4, 'Allergic reaction', 'Antihistamines', 'Allergic reaction due to food prescribed antihistamine'),
 (10, 10, '2023-05-20 00:00:00', 6, 'Conjunctivitis', 'Eye drops', 'Prescribed eye drops for conjunctivitis');
 
+ALTER TABLE owners
+ADD RegisteredDate DATE;
